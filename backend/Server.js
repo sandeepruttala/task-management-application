7f2e6cf0ed160cb2204f72f47cd91056947c8802
Sandeep Ruttala <itssandeepruttala@gmail.com>
2025-02-15 09:16:51 +0530

    updated origins

--- backend/Server.js
+++ backend/Server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-management-application-chi-navy.vercel.app/",
     credentials: true,
   })
 );
